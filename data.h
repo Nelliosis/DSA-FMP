f8a9886946d1540b62b6b28ff94c9e7c24e2f5bd
KapitanMarbel <marbellaaustingyan@gmail.com>
2021-03-15 09:23:36 +0800

    added display function renamed bool desicion() to bool decision()

--- data.h
+++ data.h
@@ -1,5 +1,6 @@
+#include <iostream>
 #include <string>
-
+#include <iomanip>
 
 using namespace std;
 
@@ -22,16 +23,41 @@ class functions
 
 public:
 
-  bool desicion(char);
+  bool decision(char);
+  void display();
 
 };
 
 //Function Definitions
 
-bool functions::desicion(char d)
+bool functions::decision(char d)
 {
   if(d == 'y' || d == 'Y')
   return true;
   else
   return false;
+}
+
+void functions::display()
+{
+	//initializes column width
+	int colwidth = 20;
+	
+	//displays headers of table
+	cout<< "<<MOVIE LIST>> \n \n";
+	cout<< left << setw(colwidth) << "Video ID" 
+	<< left << setw(colwidth) << "Movie Title" 
+	<< left << setw(colwidth) << "Genre" 
+	<< left << setw(colwidth) << "Production" 
+	<< left << setw(colwidth) << "Copies\n";
+	
+	//for loop for displaying (di ko pa sure pano mamodify to kasi wala pang data initialized)
+				/*for (int i=0; i < 10; i++){
+					cout << setw(colwidth) << emp[i].num;
+					cout << setw(colwidth) << emp[i].pos; 
+					cout << setw(colwidth) << emp[i].dept; 
+					cout << setw(colwidth) << emp[i].pr_date;
+					cout << setw(colwidth) << emp[i].net <<"\n";
+				}*/
+				cout << endl;
 }
